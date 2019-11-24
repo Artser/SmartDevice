876a921eb1ae1dab00e43b505bd53001a6837041
Artser <artserlisa@mail.ru>
2019-11-24 16:00:38 +0300

    добавляет js

---
+++ build/js/main.js
@@ -0,0 +1,27 @@
+("use strict");
+(function() {
+  var modal1 = document.querySelector("#callback");
+  var jsBackCall = document.querySelector("#js-back-call");
+  var model1Close = document.querySelector("#js-modal-close");
+  var jsModalName = document.querySelector("#js-modal-name");
+
+  document.addEventListener("click", function(evt) {
+    if (!modal1.contains(evt.target) && !jsBackCall.contains(evt.target)) {
+      modal1.style.display = "none";
+    }
+  });
+
+  jsBackCall.addEventListener("click", function() {
+    modal1.style.display = "block";
+    jsModalName.focus();
+  });
+  model1Close.addEventListener("click", function() {
+    modal1.style.display = "none";
+  });
+
+  document.addEventListener("keydown", function(evt) {
+    if (evt.key === "Escape") {
+      modal1.style.display = "none";
+    }
+  });
+})();
