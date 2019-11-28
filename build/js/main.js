17920dbea82ed0e8ef242553df5214f02baf76c4
Artser <artserlisa@mail.ru>
2019-11-28 17:15:24 +0300

    добавляет форму в подвале

--- build/js/main.js
+++ build/js/main.js
@@ -1,38 +1,34 @@
-("use strict");
-(function() {
-  var modal1 = document.querySelector("#callback");
-  var jsBackCall = document.querySelector("#js-back-call");
-  var model1Close = document.querySelector("#js-modal-close");
-  var jsModalName = document.querySelector("#js-modal-name");
-  var back = document.querySelector("#back");
+('use strict');
+(function () {
+  var modal1 = document.querySelector('#callback');
+  var jsBackCall = document.querySelector('#js-back-call');
+  var model1Close = document.querySelector('#js-modal-close');
+  var jsModalName = document.querySelector('#js-modal-name');
+  var back = document.querySelector('#back');
 
-
-  document.addEventListener("click", function(evt) {
+  document.addEventListener('click', function (evt) {
     if (!modal1.contains(evt.target) && !jsBackCall.contains(evt.target)) {
-      modal1.style.display = "none";
-      back.style.display = "none";
-
+      modal1.style.display = 'none';
+      back.style.display = 'none';
     }
   });
 
-  jsBackCall.addEventListener("click", function() {
-    modal1.style.display = "block";
-    back.style.display = "block";
+  jsBackCall.addEventListener('click', function () {
+    modal1.style.display = 'block';
+    back.style.display = 'block';
 
     jsModalName.focus();
   });
 
-  model1Close.addEventListener("click", function() {
-    modal1.style.display = "none";
-    back.style.display = "none";
-
+  model1Close.addEventListener('click', function () {
+    modal1.style.display = 'none';
+    back.style.display = 'none';
   });
 
-  document.addEventListener("keydown", function(evt) {
-    if (evt.key === "Escape") {
-      modal1.style.display = "none";
-      back.style.display = "none";
-
+  document.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Escape') {
+      modal1.style.display = 'none';
+      back.style.display = 'none';
     }
   });
 })();
