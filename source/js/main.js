6bd6ded896e642dba5d0ec8888dc586797fd0b03
Artser <artserlisa@mail.ru>
2019-12-05 08:41:18 +0300

    добавляет маску

--- source/js/main.js
+++ source/js/main.js
@@ -1,37 +1,45 @@
-('use strict');
-(function () {
-  var modal1 = document.querySelector('#callback');
-  var jsBackCall = document.querySelector('#js-back-call');
-  var model1Close = document.querySelector('#js-modal-close');
-  var jsModalName = document.querySelector('#js-modal-name');
-  var back = document.querySelector('#back');
+("use strict");
+(function() {
+  var modal1 = document.querySelector("#callback");
+  var jsBackCall = document.querySelector("#js-back-call");
+  var model1Close = document.querySelector("#js-modal-close");
+  var jsModalName = document.querySelector("#js-modal-name");
+  var back = document.querySelector("#back");
 
-  // var phoneMask =   document.querySelector('#feedback-phone'), {
-  //     mask: '+{7}(000)000-00-00'
-  //   });
-  document.addEventListener('click', function (evt) {
+  document.addEventListener("click", function(evt) {
     if (!modal1.contains(evt.target) && !jsBackCall.contains(evt.target)) {
-      modal1.style.display = 'none';
-      back.style.display = 'none';
+      modal1.style.display = "none";
+      back.style.display = "none";
+      document.body.classList.remove("modal_scrool");
     }
   });
 
-  jsBackCall.addEventListener('click', function () {
-    modal1.style.display = 'block';
-    back.style.display = 'block';
-
+  jsBackCall.addEventListener("click", function() {
+    modal1.style.display = "block";
+    back.style.display = "block";
+    document.body.classList.add("modal_scrool");
     jsModalName.focus();
   });
 
-  model1Close.addEventListener('click', function () {
-    modal1.style.display = 'none';
-    back.style.display = 'none';
+  model1Close.addEventListener("click", function() {
+    modal1.style.display = "none";
+    back.style.display = "none";
+    document.body.classList.remove("modal_scrool");
   });
 
-  document.addEventListener('keydown', function (evt) {
-    if (evt.key === 'Escape') {
-      modal1.style.display = 'none';
-      back.style.display = 'none';
+  document.addEventListener("keydown", function(evt) {
+    if (evt.key === "Escape") {
+      modal1.style.display = "none";
+      back.style.display = "none";
+      document.body.classList.remove("modal_scrool");
     }
+  });
+
+  var phoneMask = IMask(document.getElementById("feedback-phone"), {
+    mask: "+{7}(000)000-00-00"
+  });
+
+  var phoneMask2 = IMask(document.getElementById("feedback-phone2"), {
+    mask: "+{7}(000)000-00-00"
   });
 })();
