257e53c3822421da0f385de3b6d716a495d4bf1b
Artser <artserlisa@mail.ru>
2019-12-06 07:03:44 +0300

    добавляет копирайт

--- build/js/main.js
+++ build/js/main.js
@@ -1,60 +1,60 @@
-("use strict");
-(function() {
-  var modal1 = document.querySelector("#callback");
-  var jsBackCall = document.querySelector("#js-back-call");
-  var model1Close = document.querySelector("#js-modal-close");
-  var jsModalName = document.querySelector("#js-modal-name");
-  var back = document.querySelector("#back");
+('use strict');
+(function () {
+  var modal1 = document.querySelector('#callback');
+  var jsBackCall = document.querySelector('#js-back-call');
+  var model1Close = document.querySelector('#js-modal-close');
+  var jsModalName = document.querySelector('#js-modal-name');
+  var back = document.querySelector('#back');
 
-  document.addEventListener("click", function(evt) {
+  document.addEventListener('click', function (evt) {
     if (!modal1.contains(evt.target) && !jsBackCall.contains(evt.target)) {
-      modal1.style.display = "none";
-      back.style.display = "none";
-      document.body.classList.remove("modal_scrool");
+      modal1.style.display = 'none';
+      back.style.display = 'none';
+      document.body.classList.remove('modal_scrool');
     }
   });
 
-  jsBackCall.addEventListener("click", function() {
-    modal1.style.display = "block";
-    back.style.display = "block";
-    document.body.classList.add("modal_scrool");
+  jsBackCall.addEventListener('click', function () {
+    modal1.style.display = 'block';
+    back.style.display = 'block';
+    document.body.classList.add('modal_scrool');
     jsModalName.focus();
   });
 
-  model1Close.addEventListener("click", function() {
-    modal1.style.display = "none";
-    back.style.display = "none";
-    document.body.classList.remove("modal_scrool");
+  model1Close.addEventListener('click', function () {
+    modal1.style.display = 'none';
+    back.style.display = 'none';
+    document.body.classList.remove('modal_scrool');
   });
 
-  document.addEventListener("keydown", function(evt) {
-    if (evt.key === "Escape") {
-      modal1.style.display = "none";
-      back.style.display = "none";
-      document.body.classList.remove("modal_scrool");
+  document.addEventListener('keydown', function (evt) {
+    if (evt.key === 'Escape') {
+      modal1.style.display = 'none';
+      back.style.display = 'none';
+      document.body.classList.remove('modal_scrool');
     }
   });
 
-  var phoneMask = IMask(document.getElementById("feedback-phone"), {
-    mask: "+{7}(000)000-00-00"
+  var phoneMask = IMask(document.getElementById('feedback-phone'), {
+    mask: '+{7}(000)000-00-00'
   });
 
- var phoneMask2 = IMask(document.getElementById("feedback-phone2"), {
-   mask: "+{7}(000)000-00-00"
- });
+  var phoneMask2 = IMask(document.getElementById('feedback-phone2'), {
+    mask: '+{7}(000)000-00-00'
+  });
 
-  var tabElement = document.querySelector(".footer-push");
+  var tabElement = document.querySelector('.footer-push');
   if (tabElement) {
-    var items = tabElement.querySelectorAll(".footer-push__i");
+    var items = tabElement.querySelectorAll('.footer-push__i');
     items.forEach(function(item) {
-      var btn = item.querySelector(".footer-push__toggle");
+      var btn = item.querySelector('.footer-push__toggle');
 
-      btn.addEventListener("click", function() {
+      btn.addEventListener('click', function () {
         items.forEach(function(item) {
-          item.classList.remove("footer-push__i--open");
+          item.classList.remove('footer-push__i--open');
         });
 
-        item.classList.add("footer-push__i--open");
+        item.classList.add('footer-push__i--open');
       });
     });
   }
